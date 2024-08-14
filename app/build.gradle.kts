0ec909c18fd11c256e7557add7ae74a709ded9f1
anggakusumo2@gmail.com <anggakusumo2@gmail.com>
2024-08-14 16:27:07 +0700

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.playsService)
+}
+
+android {
+    namespace = "com.angga.cloudinary"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.angga.cloudinary"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    //hilt
+    implementation(libs.dagger.hilt)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    ksp(libs.dagger.hilt.compiler)
+
+    //sharedelement
+    implementation(libs.androidx.compose.animation)
+
+    //encryptedPref
+    implementation(libs.androidx.security.crypto.ktx)
+
+    //ktor
+    implementation(libs.bundles.ktor)
+
+    //coil
+    implementation(libs.coil)
+
+    //room
+    implementation(libs.room)
+    annotationProcessor(libs.room.runtime)
+    implementation(libs.room.runtime)
+    implementation(libs.room.paging)
+    ksp(libs.room.compiler)
+
+    //navigation
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.compose)
+
+    //paging
+    implementation(libs.paging.compose)
+    implementation(libs.paging.runtime)
+
+    //timber
+    implementation(libs.timber)
+    
+    //splash
+    implementation(libs.androidx.core.splashscreen)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
